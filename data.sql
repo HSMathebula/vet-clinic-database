8264ffed0ed7b0c840dd79bdd4d5ae26777cbb61
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-07-21 12:47:30 +0200

    Insert data into specializations

--- data.sql
+++ data.sql
@@ -51,3 +51,11 @@ VALUES ('Vet William Tatcher', 45, 'Apr 23 2000'),
 ('Vet Maisy Smith', 26 ,'Jan 17 2019'),
 ('Vet Stephanie Mendez', 64, 'May 04 1981'),
 ('Vet Jack Harkness', 38,'Jun 08 2008');
+
+-- insert data into specializations
+INSERT INTO specializations(species_id, vets_id)
+VALUES ((SELECT id FROM species WHERE name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'William Tatcher')),
+
+((SELECT id FROM species WHERE name = 'Digimon' OR name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'Vet Stephanie Mendez')),
+
+((SELECT id FROM species WHERE name = 'Digimon'), (SELECT id FROM vets WHERE name = 'Vet Jack Harkness'));
